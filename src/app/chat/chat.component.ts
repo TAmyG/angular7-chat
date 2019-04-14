00b05f8fcc7f66c7c081967f6c85a284eca95824
TAmy <tamy.vivas@gmail.com>
2019-04-13 20:05:30 -0600

    feat: Implemented clienteId and get historial from server via webSocket

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -14,8 +14,17 @@ export class ChatComponent implements OnInit {
   mensaje: Mensaje = new Mensaje();
   mensajes: Mensaje[] = [];
   escribiendo: string;
+  clienteId: string;
 
-  constructor() {}
+  constructor() {
+    this.clienteId =
+      'id-' +
+      new Date().getTime() +
+      '-' +
+      Math.random()
+        .toString(36)
+        .substr(2);
+  }
 
   ngOnInit() {
     this.client = new Client();
@@ -48,6 +57,21 @@ export class ChatComponent implements OnInit {
         setTimeout(() => (this.escribiendo = ''), 3000);
       });
 
+      this.client.subscribe('/chat/historial/' + this.clienteId, e => {
+        const historial = JSON.parse(e.body) as Mensaje[];
+        this.mensajes = historial
+          .map(m => {
+            m.fecha = new Date(m.fecha);
+            return m;
+          })
+          .reverse();
+      });
+
+      this.client.publish({
+        destination: '/app/historial',
+        body: this.clienteId
+      });
+
       this.mensaje.tipo = 'NUEVO_USUARIO';
       this.client.publish({
         destination: '/app/mensaje',
@@ -58,6 +82,8 @@ export class ChatComponent implements OnInit {
     this.client.onDisconnect = frame => {
       console.log('Desconectados: ' + !this.client.connected + ' : ' + frame);
       this.conectado = false;
+      this.mensaje = new Mensaje();
+      this.mensajes = [];
     };
   }
 
